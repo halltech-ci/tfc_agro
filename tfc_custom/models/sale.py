0212f74a94ab19b9f4b4a4aac46b069d00a06bf3
Maurice ATCHE <maurice.atche@halltech-africa.com>
2020-02-22 11:10:55 +0000

    Added lot_in in sale order line

--- tfc_custom/models/sale.py
+++ tfc_custom/models/sale.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api, _
+from odoo.exceptions import UserError
 
 # class tfc_custom(models.Model):
 #     _name = 'tfc_custom.tfc_custom'
@@ -24,7 +25,57 @@ class SaleOrder(models.Model):
     driver_contacts=fields.Char(string="Driver Contact")
     customer_order_ref=fields.Char(string="Customer Order Ref")
     
+    #Lot methods
+    @api.model
+    def get_move_from_line(self, line):
+        move = self.env['stock.move']
+        # i create this counter to check lot's univocity on move line
+        lot_count = 0
+        for p in line.order_id.picking_ids:
+            for m in p.move_lines:
+                move_line_id = m.move_line_ids.filtered(
+                    lambda line: line.lot_id)
+                if move_line_id and line.lot_id == move_line_id[:1].lot_id:
+                    move = m
+                    lot_count += 1
+                    # if counter is 0 or > 1 means that something goes wrong
+                    if lot_count != 1:
+                        raise UserError(_('Can\'t retrieve lot on stock'))
+        return move
+
+    @api.model
+    def _check_move_state(self, line):
+        if line.lot_id:
+            move = self.get_move_from_line(line)
+            if move.state == 'confirmed':
+                move._action_assign()
+                move.refresh()
+            if move.state != 'assigned':
+                raise UserError(_('Can\'t reserve products for lot %s') %
+                                line.lot_id.name)
+        return True
+
+    @api.multi
+    def action_confirm(self):
+        res = super(SaleOrder, self.with_context(sol_lot_id=True))\
+            .action_confirm()
+        for line in self.order_line:
+            if line.lot_id:
+                unreserved_moves = line.move_ids.filtered(
+                    lambda move: move.product_uom_qty !=
+                    move.reserved_availability
+                )
+                if unreserved_moves:
+                    raise UserError(
+                        _('Can\'t reserve products for lot %s')
+                        % line.lot_id.name
+                    )
+            self._check_move_state(line)
+        return res
     
+    
+    
+    #Inherit create method to add custom sequence in sale order
     @api.model
     def create(self, vals):
         if vals.get('name', _('New')) == _('New'):
@@ -42,3 +93,36 @@ class SaleOrder(models.Model):
             vals['pricelist_id'] = vals.setdefault('pricelist_id', partner.property_product_pricelist and partner.property_product_pricelist.id)
         result = super(SaleOrder, self).create(vals)
         return result
+    
+class SaleOrderLine(models.Model):
+    _inherit='sale.order.line'
+    
+    lot_id=fields.Many2one('stock.production.lot', string='Lot', copy=False)
+    
+    @api.multi
+    @api.onchange('product_id')
+    def product_id_change(self):
+        super(SaleOrderLine, self).product_id_change()
+        self.lot_id = False#On itialise la valeur de lot_id a False
+        
+    @api.onchange('product_id')
+    def _onchange_product_id_set_lot_domain(self):
+        available_lot_ids=[]#On itialise la liste des lots disponible
+            
+        #Si le produit existe et il existe un entrepot pour le devis
+        if self.product_id and self.order_id.warehouse_id:
+            #Je recupere le nom du stock
+            location = self.order_id.warehouse_id.lot_stock_id
+            quants = self.env['stock.quant'].read_group([
+                ('product_id', '=', self.product_id.id),
+                ('location', 'child_of', location.id),
+                ('quantity', '>', 0),
+                ('lot_id', '!=', False),
+                    
+                ], ['lot_id'], 'lot_id')
+            available_lot_ids = [quant['lot_id'][0] for quant in quants]
+        self.lot_id = False
+        return {
+            'domain':{'lot_id':[('id', 'in', available_lot_ids)]}
+        }
+    
