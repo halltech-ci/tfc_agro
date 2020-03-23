c7056716d81fa5259e3a675d3f1057f2284e18f8
Maurice ATCHE <maurice.atche@halltech-africa.com>
2020-03-23 01:05:30 +0000

    Added custom report stock position

--- tfc_custom/reports/custom_report.py
+++ tfc_custom/reports/custom_report.py
@@ -2,7 +2,8 @@
 
 from odoo import models, fields, api
 from datetime import datetime
-from odoo.tools import DEFAULT_SERVER_DATE_FORMAT as DATE_FORMAT, DEFAULT_SERVER_DATETIME_FORMAT, date_utils
+from odoo.tools import DEFAULT_SERVER_DATE_FORMAT as DATE_FORMAT, DEFAULT_SERVER_DATETIME_FORMAT
+from odoo.tools import date_utils
 
 
 # class tfc_custom(models.Model):
@@ -21,18 +22,39 @@ class CustomReport(models.AbstractModel):
     _name="report.tfc_custom.custom_report_template"#Respect naming format report.module_name.report_template_name
     _description="Custom report for TFC AGRO"
     
+    def _get_received_qty(self, product_id, date):
+        moves = self.env['stock.move'].search([('picking_code', '=', 'incoming'), ('product_id.id', '=', product_id), ('product_type', '=', 'product')])#Get all incoming picking
+        total_recept = 0.0
+        total_transfert = 0.0
+        for move in moves:
+            if move.write_date.date() == date:
+                if move.picking_code == 'incoming':
+                    total_recept = total_recept + move.quantity_done
+                if move.picking_code == 'internal':
+                    total_transfert += total_transfert 
+                total_recept = total_recept
+                total_transfert = total_transfert
+        return (total_recept, total_transfert)
+    
+    #Get rebaggage quantity per product
+    def _get_rebaggage_qty(self, product_id, date):
+        conversions = self.env['product.conversion'].search([('src_product_id.id', '=', product_id), ('state', '=', 'done')])
+        rebaggage = 0.0
+        for conversion in conversions:
+            if conversion.date == date:
+                rebaggage += conversion.qty_to_convert
+            rebaggage = rebaggage
+            return rebaggage
     
     @api.model
     def _get_report_values(self, docids, data=None):
         
         date_from = data['form']['date_from']
         date_to = data['form']['date_to']
+        date_to_obj = datetime.strptime(date_to, DATE_FORMAT).date()
+        #date = date_utils.to_date(date_to)
         
         docs = []
-        #stock.quant give product quantity in on hands at any time
-        #ref_datetime = date_utils.datetime(date_from, 'day')#Get date initial datetime
-        #quants_from = self.env['product.product'].search([('type', '=', product), ('write_date', '<=', ref_datetime)])
-        #quants_to = self.env['product.product'].search([('type', '=', product), ('write_date', '>=', date_to)])
         quants = self.env['product.product'].search([('type', '=', 'product')])
         #accountmove_line = self.env['account.move.line'].search([()])
         for quant in quants :
@@ -46,8 +68,10 @@ class CustomReport(models.AbstractModel):
                 'delivery_order': quant.outgoing_qty, #qty waiting for delivery
                 'recept_order': quant.incoming_qty, #qty purchase and waiting for reception
                 'virtual_available': quant.virtual_available, #forcasted (prevision) qty
-                'initial_qty': quant.qty_at_date + quant.sales_count,
-                'recept_qty':0, #Qty of product recept in that day
+                'initial_qty': quant.qty_at_date + quant.sales_count - self._get_received_qty(quant.id, date_to_obj)[0],
+                'recept_qty': self._get_received_qty(quant.id, date_to_obj)[0], #Qty of product recept in that day
+                'internal_transfert': self._get_received_qty(quant.id, date_to_obj)[1],
+                'rebaggage':  self._get_rebaggage_qty(quant.id, date_to_obj),
                 #if 
             })
     
