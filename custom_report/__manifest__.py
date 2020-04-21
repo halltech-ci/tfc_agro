a75c0dacece90748c8512803333d08fab53227d3
Maurice ATCHE <maurice.atche@halltech-africa.com>
2020-04-21 20:59:29 +0000

    Add model custom report

--- custom_report/__manifest__.py
+++ custom_report/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "tfc_custom",
+    'name': "custom_report",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
@@ -20,34 +20,16 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base',
-               'product',
-               'purchase',
-               'sale_management',
-               'sale_stock',
-                'account_accountant',
-               ],
+    'depends': ['base'],
 
     # always loaded
     'data': [
-        #'security/ir.model.access.csv',
+        # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
-        #'views/product_template_views.xml',
-        'views/sale_order_views.xml',
-        'reports/sale_order_report.xml',
-        'reports/purchase_order_report.xml',
-        'reports/account_invoice_report.xml',
-        'data/sale_ir_sequence.xml',
-        'data/purchase_ir_sequence.xml',
-        'views/stock_picking_views.xml',
-        #'data/stock_picking_ir_sequence.xml',
-        'reports/stock_picking_report.xml',
-        'views/account_invoice_views.xml',
-        'views/purchase_order_views.xml',
         #'views/res_partner_views.xml',
-        #'wizards/create_custom_report.xml',
-        #'reports/custom_report.xml',
+        'wizards/create_custom_report.xml',
+        'reports/custom_report.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
