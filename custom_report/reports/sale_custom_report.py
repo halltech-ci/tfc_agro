23529748cf3c08a776c8d35922d24a1abb3e26b5
Maurice ATCHE <maurice.atche@halltech-africa.com>
2020-06-19 01:44:49 +0000

    Add filter in custom report

--- custom_report/reports/sale_custom_report.py
+++ custom_report/reports/sale_custom_report.py
@@ -13,12 +13,21 @@ class SaleOrderLine(models.Model):
     #@api.depends("lot_id")
     def _compute_lot_name(self):
         if self.lot_id:
-            self.lot = self.lot_id
+            self.lot = self.lot_id.name
         else:
             self.lot = " "
     
-    lot = fields.Char(string="Lot_id", compute="_compute_lot_name")
-    confirmation_date = fields.Datetime(string="Date Order", default= lambda self : self.order_id.confirmation_date)
+    @api.one
+    #@api.depends("lot_id")
+    def _compute_payment_term(self):
+        if self.order_id.payment_term_id:
+            self.payment_term = self.order_id.payment_term_id.name
+        else:
+            self.payment_term = " "
+    
+    lot = fields.Char(string="Vessel", compute="_compute_lot_name")
+    payment_term = fields.Char(string="Payment Term", compute="_compute_payment_term")
+    date_order = fields.Datetime(string="Date Order", related="order_id.date_order", store=True)
     
     
             
