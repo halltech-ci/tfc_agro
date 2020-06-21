f460480f130cb498a13e0fe3de81b79bc7d10f23
Maurice ATCHE <maurice.atche@halltech-africa.com>
2020-06-21 02:02:48 +0000

    Add custom sequence in stock picking

--- tfc_custom/models/sale.py
+++ tfc_custom/models/sale.py
@@ -146,9 +146,9 @@ class SaleOrder(models.Model):
     def create(self, vals):
         if vals.get('name', _('New')) == _('New'):
             if 'company_id' in vals:
-                vals['name'] = self.env['ir.sequence'].with_context(force_company=vals['company_id']).next_by_code('sale.order.sequence') or _('New')
+                vals['name'] = self.env['ir.sequence'].with_context(force_company=vals['company_id']).next_by_code('sale.adl.sequence') or _('New')
             else:
-                vals['name'] = self.env['ir.sequence'].next_by_code('sale.order.sequence') or _('New')
+                vals['name'] = self.env['ir.sequence'].next_by_code('sale.adl.sequence') or _('New')
 
         # Makes sure partner_invoice_id', 'partner_shipping_id' and 'pricelist_id' are defined
         if any(f not in vals for f in ['partner_invoice_id', 'partner_shipping_id', 'pricelist_id']):
--- tfc_custom/models/stock_picking.py
+++ tfc_custom/models/stock_picking.py
@@ -14,7 +14,7 @@ from odoo import models, fields, api
 #     def _value_pc(self):
 #         self.value2 = float(self.value) / 100
 
-
+picking_code = ['outgoing', 'incoming', 'internal']
 class StockPicking(models.Model):
     _inherit="stock.picking"
     
@@ -24,3 +24,56 @@ class StockPicking(models.Model):
     driver_company = fields.Char(string="Driver Company")
     chargeur = fields.Char(string="Chargeur")
     
+    
+    
+    @api.model
+    def create(self, vals):
+        # TDE FIXME: clean that brol
+        defaults = self.default_get(['name', 'picking_type_id'])
+        if vals.get('name', '/') == '/' and defaults.get('name', '/') == '/' and vals.get('picking_type_id', defaults.get('picking_type_id')):
+            #vals['name'] = self.env['ir.sequence'].next_by_code('stock.outgoing.sequence') or _('/')
+            if not vals.get('sale_id', False):
+                vals['name'] = self.env['ir.sequence'].next_by_code('stock.outgoing.sequence') or _('/')
+            if not vals.get('purchase_id', False):
+                vals['name'] = self.env['ir.sequence'].next_by_code('stock.incoming.sequence') or _('/')
+            else:
+                vals['name'] = self.env['ir.sequence'].next_by_code('stock.internal.sequence') or _('/')
+            #self.env['ir.sequence'].next_by_code('sale.order.sequence') or _('New')#['stock.picking.type'].browse(vals.get('picking_type_id', defaults.get('picking_type_id'))).sequence_id.next_by_code('stock.outgoing.sequence')
+
+        # TDE FIXME: what ?
+        # As the on_change in one2many list is WIP, we will overwrite the locations on the stock moves here
+        # As it is a create the format will be a list of (0, 0, dict)
+        if vals.get('location_id') and vals.get('location_dest_id'):
+            for move in vals.get('move_lines', []) + vals.get('move_ids_without_package', []):
+                if len(move) == 3 and move[0] == 0:
+                    move[2]['location_id'] = vals['location_id']
+                    move[2]['location_dest_id'] = vals['location_dest_id']
+        res = super(StockPicking, self).create(vals)
+        res._autoconfirm_picking()
+        return res
+
+    
+    '''
+    @api.model
+    def create(self, vals):
+        # TDE FIXME: clean that brol
+        #defaults = self.default_get(['name', 'picking_type_id'])
+        if vals.get('name', '/') == '/':
+            if vals.get('stock_picking_code') in picking_code:
+                code = vals.get('stock_picking_code')
+                sequence_code = '{0}.{1}.{2}'.format('stock', code, 'sequence')
+                vals['name'] = self.env['ir.sequence'].sequence_id.next_by_code(sequence_code)
+
+        # TDE FIXME: what ?
+        # As the on_change in one2many list is WIP, we will overwrite the locations on the stock moves here
+        # As it is a create the format will be a list of (0, 0, dict)
+        if vals.get('location_id') and vals.get('location_dest_id'):
+            for move in vals.get('move_lines', []) + vals.get('move_ids_without_package', []):
+                if len(move) == 3 and move[0] == 0:
+                    move[2]['location_id'] = vals['location_id']
+                    move[2]['location_dest_id'] = vals['location_dest_id']
+        res = super(StockPicking, self).create(vals)
+        res._autoconfirm_picking()
+        return res
+    '''
+
