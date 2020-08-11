98729adf7cb7e230981c371f69ea9358308248da
ATCHE Maurice <maurice.atche@halltech-africa.com>
2020-08-11 23:07:51 +0000

    Add check account in res_config

---
+++ custom_report/models/res_company.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+# class custom_report(models.Model):
+#     _name = 'custom_report.custom_report'
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         self.value2 = float(self.value) / 100
+
+
+class ResCompany(models.Model):
+    _inherit = 'res.company'
+    
+    pdc_check_account = fields.Many2one("account.journal", string="PDC Account")
+    check_on_hand_journal = fields.Many2one("account.journal", string="Check On Hand Account")
+    check_on_bank_journal = fields.Many2one("account.journal", string="Check on Bank Account")
