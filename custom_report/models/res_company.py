d3abb9dd5fd6159f7b75dc155c2e337f5217390e
ATCHE Maurice <maurice.atche@halltech-africa.com>
2020-08-12 09:29:56 +0000

    Add res_config_setting for check account

--- custom_report/models/res_company.py
+++ custom_report/models/res_company.py
@@ -18,6 +18,6 @@ from odoo import models, fields, api
 class ResCompany(models.Model):
     _inherit = 'res.company'
     
-    pdc_check_account = fields.Many2one("account.journal", string="PDC Account")
-    check_on_hand_journal = fields.Many2one("account.journal", string="Check On Hand Account")
-    check_on_bank_journal = fields.Many2one("account.journal", string="Check on Bank Account")
+    pdc_check_account = fields.Many2one("account.account", string="PDC Account")
+    check_on_hand_journal = fields.Many2one("account.account", string="Check On Hand Account")
+    check_on_bank_journal = fields.Many2one("account.account", string="Check on Bank Account")
