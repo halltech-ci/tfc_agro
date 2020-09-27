51bec5d49c283a9eff12ca747d32c7b8d38e8479
ATCHE Maurice <maurice.atche@halltech-africa.com>
2020-09-27 02:43:21 +0000

    accounting_pdf adding my custom partner ledger

--- accounting_pdf_reports/reports/report_partner_ledger.py
+++ accounting_pdf_reports/reports/report_partner_ledger.py
@@ -15,7 +15,7 @@ class ReportPartnerLedger(models.AbstractModel):
         reconcile_clause = "" if data['form']['reconciled'] else ' AND "account_move_line".full_reconcile_id IS NULL '
         params = [partner.id, tuple(data['computed']['move_state']), tuple(data['computed']['account_ids'])] + query_get_data[2]
         query = """
-            SELECT "account_move_line".id, "account_move_line".date, j.code, acc.code as a_code, acc.name as a_name, "account_move_line".ref, m.name as move_name, "account_move_line".name, "account_move_line".debit, "account_move_line".credit, "account_move_line".amount_currency,"account_move_line".currency_id, c.symbol AS currency_code
+            SELECT "account_move_line".id as line_id, "account_move_line".date, j.code, acc.code as a_code, acc.name as a_name, "account_move_line".ref, m.name as move_name, "account_move_line".name, "account_move_line".debit, "account_move_line".credit, "account_move_line".amount_currency,"account_move_line".currency_id, c.symbol AS currency_code
             FROM """ + query_get_data[0] + """
             LEFT JOIN account_journal j ON ("account_move_line".journal_id = j.id)
             LEFT JOIN account_account acc ON ("account_move_line".account_id = acc.id)
@@ -33,6 +33,7 @@ class ReportPartnerLedger(models.AbstractModel):
         lang_id = lang._lang_get(lang_code)
         date_format = lang_id.date_format
         for r in res:
+            r['line_id'] = r['line_id']
             r['date'] = r['date']
             r['displayed_name'] = '-'.join(
                 r[field_name] for field_name in ('move_name', 'ref', 'name')
@@ -42,7 +43,13 @@ class ReportPartnerLedger(models.AbstractModel):
             r['progress'] = sum
             r['currency_id'] = currency.browse(r.get('currency_id'))
             full_account.append(r)
-        return full_account
+        return full_account# = ['line_id', 'date', 'display_name', 'progess', 'currency_id']
+    
+    def _get_line_detail(self, line_id):
+        lines = self.env['account.move.line'].search([('id', '=', line_id)]).move_id#.line_ids
+        #move_line = move.search([(line)])
+        return lines
+        
 
     def _sum_partner(self, data, partner, field):
         if field not in ['debit', 'credit', 'debit - credit']:
@@ -117,4 +124,5 @@ class ReportPartnerLedger(models.AbstractModel):
             'time': time,
             'lines': self._lines,
             'sum_partner': self._sum_partner,
+            'get_line_detail': self._get_line_detail
         }
